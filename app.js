61ba673337f78e4c306cac9c6419b1b3a167063a
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2021-11-09 15:09:12 +0000

    query selector & all

--- app.js
+++ app.js
@@ -5,21 +5,34 @@ var bookList = document.getElementById('book-list');
 
 
 
-var titles = document.getElementsByClassName('title');
-console.log(titles);
+// var titles = document.getElementsByClassName('title');
+// console.log(titles);
 
 
-var lis = document.getElementsByTagName('li');
-console.log(lis);
+// var lis = document.getElementsByTagName('li');
+// console.log(lis);
 
 
-for(i=0;i<titles.length;i++){
-  console.log(titles[i])
-}
+// for(i=0;i<titles.length;i++){
+//   console.log(titles[i])
+// }
 
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
 
-Array.from(titles).forEach(function(item){
-  console.log(item)
+// Array.from(titles).forEach(function(item){
+//   console.log(item)
+// });
+
+
+var books = document.querySelector('#book-list li:nth-child(2)');
+console.log(books);
+var booky = document.querySelector('#book-list li .name');
+console.log(booky);
+booky = document.querySelectorAll('#book-list li .name');
+console.log(booky);
+
+
+Array.from(booky).forEach(function(book){
+  console.log(book)
 });
