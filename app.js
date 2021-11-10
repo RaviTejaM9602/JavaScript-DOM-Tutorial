a9ce050e7091a8840d1c6b14237ce97ece7b0593
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2021-11-10 14:19:13 +0000

    creating new elements

--- app.js
+++ app.js
@@ -59,3 +59,26 @@ addForm.addEventListener('submit', function(e){
   const value = addForm.querySelector('input[type="text"]').value;
   console.log(value);
 });
+
+
+// add books
+const addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  // create elements
+  const value = addForm.querySelector('input[type="text"]').value;
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // add text content
+  bookName.textContent = value;
+  deleteBtn.textContent = 'delete';
+
+  // append to DOM
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+  //list.insertBefore(li, list.querySelector('li:first-child'));
+});
