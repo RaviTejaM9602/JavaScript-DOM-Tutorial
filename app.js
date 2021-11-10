a14cfcb5b61782add97db777040ac0ead7b66c8d
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2021-11-10 13:48:32 +0000

    event

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ console.log(bookList.childNode);
 console.log(bookList.nextSibling);
 console.log(bookList.nextElementSibling);
 
+const list = document.querySelector('#book-list ul');
+const listItems = document.querySelectorAll('#book-list ul li');
+
+Array.from(listItems).forEach(function(item){
+  item.addEventListener('click', (e) => {
+
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    console.log('child element to remove:', li);
+    console.log('parent element to remove child from:', li.parentElement);
+    li.parentNode.removeChild(li);
+  }
+
+  });
+});
---
+++ event.js
@@ -0,0 +1,5 @@
+var h2 = document.querySelector('#book-list h2');
+h2.addEventListener('click', function(e){
+  console.log(e.target);
+  console.log(e);
+});
